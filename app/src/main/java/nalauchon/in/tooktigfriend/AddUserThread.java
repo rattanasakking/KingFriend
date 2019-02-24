0382911fdd3b9a22e849a52712f9f618359baf31
rattanasak <rattanasak.king@gmail.com>
2019-02-24 15:28:07 +0700

    Update 2019/2/24:15.27

--- app/src/main/java/nalauchon/in/tooktigfriend/AddUserThread.java
+++ app/src/main/java/nalauchon/in/tooktigfriend/AddUserThread.java
@@ -3,12 +3,18 @@ package nalauchon.in.tooktigfriend;
 import android.content.Context;
 import android.os.AsyncTask;
 
-public class AddUserThread extends AsyncTask<String,Void,String> {
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+public class AddUserThread extends AsyncTask<String, Void, String> {
 
     private Context context;
 
     public AddUserThread(Context context) {
-        this.context =context;
+        this.context = context;
 
     }
 
@@ -17,18 +23,22 @@ public class AddUserThread extends AsyncTask<String,Void,String> {
     protected String doInBackground(String... strings) {
 
         try {
-
-            
-
+            OkHttpClient okHttpClient = new OkHttpClient();
+            RequestBody requestBody = new FormEncodingBuilder()
+                    .add("isAdd", "true")
+                    .add("Name", strings[0])
+                    .add("User", strings[1])
+                    .add("Password", strings[2])
+                    .add("Avatar", strings[3])
+                    .build();
+            Request.Builder builder = new Request.Builder();
+            Request request = builder.url("").post(requestBody).build();
+            Response response = okHttpClient.newCall(request).execute();
 
         } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
 
-
-
-
-
-        return null;
     }
 }
