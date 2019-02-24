32d1f891e235491f5886b7a4b911e50b3bdce2be
rattanasak <rattanasak.king@gmail.com>
2019-02-24 11:18:11 +0700

    Update 2019/2/24:11.17

--- app/src/main/java/nalauchon/in/tooktigfriend/RegisterFragment.java
+++ app/src/main/java/nalauchon/in/tooktigfriend/RegisterFragment.java
@@ -1,13 +1,18 @@
 package nalauchon.in.tooktigfriend;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 /**
@@ -15,6 +20,9 @@ import android.view.ViewGroup;
  */
 public class RegisterFragment extends Fragment {
 
+//    Explicit
+    private boolean aBoolean =true;
+    private ImageView imageView;
 
     public RegisterFragment() {
         // Required empty public constructor
@@ -25,6 +33,67 @@ public class RegisterFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
 
 //        Create Toolbar
+        createToolbar();
+
+        //        Choose Image
+        chooseImage();
+
+    }   // Main Method
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        
+    }
+
+    private void chooseImage() {
+        imageView = getView().findViewById(R.id.imvAvata);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                aBoolean = false;
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "เลือกรูปจากแอพพลิเคชั่น"),1);
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.itemUpload) {
+
+            checkValue();
+
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    private void checkValue() {
+
+        MyAlert myAlert = new MyAlert(getActivity());
+
+
+
+        if (aBoolean) {
+//                Non Choose Image
+            myAlert.normalDialog("ไม่พบรูปภาพของคุณ", "กรุณาอัพโหลดรูปภาพ");
+        }
+
+    }   //checkValue
+
+    //  สร้างเมนู
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_register, menu);
+    }
+
+    private void createToolbar() {
         Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
         ((MainActivity)getActivity()).setSupportActionBar(toolbar);
         ((MainActivity)getActivity()).getSupportActionBar().setTitle("Register");
@@ -36,10 +105,8 @@ public class RegisterFragment extends Fragment {
                 getActivity().getSupportFragmentManager().popBackStack();
             }
         });
-
-
-
-    }   // Main Method
+        setHasOptionsMenu(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
